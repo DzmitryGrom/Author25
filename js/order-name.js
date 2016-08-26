0b0887c60d3cb4ddd9a2a0ab1b701d097476e533
Denis Narush <child.denis@gmail.com>
2016-08-26 08:52:02 +0300

    added bonus and two btn

--- js/order-name.js
+++ js/order-name.js
@@ -11,5 +11,20 @@ $(document).ready(function () {
             $(this).html("Подробнее");
         }
     });
-
+    $(".btn-message").on("click", function () {
+        $(this).parent().next().toggleClass("show");
+        if ($('.content-order-name-offers-list-item-author-info-message').hasClass("show")) {
+            $(this).html("<i class='fa fa-chevron-down'></i>Сообщения");
+        } else {
+            $(this).html("<i class='fa fa-chevron-up'></i>Сообщения");
+        }
+    })
+    $(".more").on("click", function(){
+        $(this).parent().parent().toggleClass("active");
+        if ($('.content-order-name-offers-list-item-author-info-bottom-panel-info').hasClass("active")) {
+            $(this).html("Свернуть");
+        } else {
+            $(this).html("Подробнее");
+        }
+    })
 });
