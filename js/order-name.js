a433c6b44881b5cde8f33d20e94bfd768425f306
DzmitryGrom <Shelegeykodmitriy@mail.ru>
2016-10-25 16:14:27 +0300

    change order-name.html and fir order-name,js

--- js/order-name.js
+++ js/order-name.js
@@ -12,22 +12,22 @@ $(document).ready(function () {
         }
     });
     $(".btn-message").on("click", function () {
-        $(this).parent().next().toggleClass("show");
-        if ($('.content-order-name-offers-list-item-author-info-message').hasClass("show")) {
+        $(this).parent().next().toggleClass("active");
+        if ($('.content-order-name-offers-list-item-author-info-message').hasClass("active")) {
             $(this).html("<i class='fa fa-chevron-down'></i>Сообщения");
         } else {
             $(this).html("<i class='fa fa-chevron-up'></i>Сообщения");
         }
-    })
-    $(".more").on("click", function(){
+    });
+    $(".more").on("click", function () {
         $(this).parent().parent().toggleClass("active");
         if ($('.content-order-name-offers-list-item-author-info-bottom-panel-info').hasClass("active")) {
             $(this).html("Свернуть");
         } else {
             $(this).html("Подробнее");
         }
-    })
-    $(".checkbox-dynamic").on("click", function(){
+    });
+    $(".checkbox-dynamic").on("click", function () {
         $(this).toggleClass("checked");
-    })
+    });
 });
